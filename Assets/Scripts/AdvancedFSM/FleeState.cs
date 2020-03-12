2d2de05db0611506d8e954b38a4c3ee9943bee5c
michielenzo <michielidema@outlook.com>
2020-03-12 22:56:11 +0100

    fleeing state boilerplate implemente.

---
+++ Assets/Scripts/AdvancedFSM/FleeState.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+
+public class FleeState: FSMState
+{
+
+    public FleeState()
+    {
+        stateID = FSMStateID.Fleeing;
+    }
+    
+    public override void Reason(Transform player, Transform npc)
+    {
+        Debug.Log("No reason to change state.");
+    }
+
+    public override void Act(Transform player, Transform npc)
+    {
+        Debug.Log("Tank is fleeing.");
+    }
+}
